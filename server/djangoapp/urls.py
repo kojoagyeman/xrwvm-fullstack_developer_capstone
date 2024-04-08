b87af9c3ef64c08a566cdb8cb49aab7e2fc94081
kojoagyeman <152467258+kojoagyeman@users.noreply.github.com>
2024-04-07 22:57:00 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from djangoapp import views
 from . import views
 
 app_name = 'djangoapp'
@@ -12,6 +13,12 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.login_user, name='logout'),
     path('register/', views.registration, name='register'),
+    path('populate_database/', views.populate_database, name='populate_database'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
   
     # path for dealer reviews view
 
