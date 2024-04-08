9c6e241668d87b25ce48e27ef761b903441f5c97
kojoagyeman <152467258+kojoagyeman@users.noreply.github.com>
2024-04-07 20:33:33 -0500

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,47 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const documents = await Dealerships.find();
+        res.json(documents);
+      } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+      }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+try {
+    const documents = await Dealerships.find({state: req.params.state});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+try {
+  
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+      return res.status(400).json({error: 'Invalid dealer id'}); 
+    }
+
+    const dealer = await Dealerships.findOne({id});
+    
+    if (!dealer) {
+      return res.status(404).json({error: 'Dealer not found'});
+    }
+
+    res.json(dealer);
+
+  } catch (error) {
+    res.status(500).json({error: 'Error fetching dealer'});
+  }
 });
 
 //Express route to insert review
