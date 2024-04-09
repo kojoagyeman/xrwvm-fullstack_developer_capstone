81cdf433631fdd28cbc48d756cd643c3b8a7cf86
kojoagyeman <152467258+kojoagyeman@users.noreply.github.com>
2024-04-08 23:32:19 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,28 +1,38 @@
-# Uncomment the imports before you add the code
-from django.urls import path
+"""djangoproj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-from djangoapp import views
-from . import views
+from djangoapp.views import get_dealerships
 
-app_name = 'djangoapp'
+
+
 urlpatterns = [
-    # # path for registration
+    path('admin/', admin.site.urls),
+    path('djangoapp/', include('djangoapp.urls')),
+    path('', TemplateView.as_view(template_name="Home.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('get_dealers/', TemplateView.as_view(template_name="index.html")),
 
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-    path(route='logout', view=views.login_user, name='logout'),
-    path('register/', views.registration, name='register'),
-    path('populate_database/', views.populate_database, name='populate_database'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    path(route='add_review', view=views.add_review, name='add_review'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-  
-    # path for dealer reviews view
+    
 
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
