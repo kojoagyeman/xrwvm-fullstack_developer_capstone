b51c819e4057b7bab8db9fc21c096938704cb41b
kojoagyeman <152467258+kojoagyeman@users.noreply.github.com>
2024-04-08 23:11:35 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','https://kojoagyeman4-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS=['https://kojoagyeman4-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+ALLOWED_HOSTS=['localhost',' https://kojoagyeman4-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS=['https://kojoagyeman4-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
