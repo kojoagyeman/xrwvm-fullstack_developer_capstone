99ac56bce4ba20b8dcc6ceea4ba0c0557b73de44
kojoagyeman <152467258+kojoagyeman@users.noreply.github.com>
2024-04-08 23:30:45 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path(route='logout', view=views.login_user, name='logout'),
     path('register/', views.registration, name='register'),
     path('populate_database/', views.populate_database, name='populate_database'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
