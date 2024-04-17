6391aeab2a9c5ec1206fc55db931c6fd3f8e340b
kojoagyeman <152467258+kojoagyeman@users.noreply.github.com>
2024-04-16 20:29:28 -0500

    Update manage.py

--- server/manage.py
+++ server/manage.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoproj.settings')
@@ -16,6 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+
 if __name__ == '__main__':
     main()
-
