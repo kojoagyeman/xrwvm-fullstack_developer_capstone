13045866ecbc65da54f629cb1e3e43d5457954fa
kojoagyeman <152467258+kojoagyeman@users.noreply.github.com>
2024-04-16 20:25:33 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -133,3 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),]
+
