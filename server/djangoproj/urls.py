e69d732b1786cc2f6a384f6a2c6e6da06565ff21
kojoagyeman <152467258+kojoagyeman@users.noreply.github.com>
2024-04-20 02:02:16 -0500

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -32,7 +32,10 @@ urlpatterns = [
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('get_dealers/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 
-    
+
+
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
